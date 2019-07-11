1554e5de2883915eb536f3cd30bc062c2b34a771
Julien Bisconti <julien.bisconti@gmail.com>
2019-07-11 14:23:57 +0200

    Basic terraform example

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+    default = "ID OF PROJECT"
+}
+
+variable "bucket_name" {
+  default = "my_bucket"
+}
+
+variable "vm_name" {
+  default = "my_vm"
+}
+
+variable "storage_bucket_roles" {
+  type = "list"
+
+  default = [
+    "roles/storage.legacyBucketReader",
+    "roles/storage.objectAdmin",
+  ]
+}
